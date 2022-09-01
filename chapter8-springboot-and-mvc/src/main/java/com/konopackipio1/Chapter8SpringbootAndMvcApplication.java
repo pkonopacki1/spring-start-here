ba82ccd688a1e26de44a46727e523ce89ce0a5b8
pkonopacki1 <konopackipio@gmail.com>
2022-09-01 12:46:43 +0200

    update commit

---
+++ chapter8-springboot-and-mvc/src/main/java/com/konopackipio1/Chapter8SpringbootAndMvcApplication.java
@@ -0,0 +1,13 @@
+package com.konopackipio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter8SpringbootAndMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter8SpringbootAndMvcApplication.class, args);
+	}
+
+}
---
+++ chapter8-springboot-and-mvc/src/test/java/com/konopackipio1/chapter8springbootandmvc/Chapter8SpringbootAndMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konopackipio1.chapter8springbootandmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter8SpringbootAndMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
